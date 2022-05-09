5723b6e7c6a27004d87dc880cf9f05eff9bc47dd
S02633737 <aanguiano6@student.cccs.edu>
2022-05-09 11:27:03 -0600

    Final Project Code Second Stage
    Second stage of the code for my project, adding the light. On my pi, I saved the different stages of my project code as different files, but here I just pasted over the original so it would show as one file being built upon in the commits.

--- Python Code/CIS 220 Final Project Code.py
+++ Python Code/CIS 220 Final Project Code.py
@@ -2,13 +2,17 @@
 from time import sleep
 
 #Third Party Imports
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor, LED
 
 sensor = MotionSensor(4)
+led = LED(17)
 
 num = 0
 while True:
     sensor.wait_for_motion()
+    led.on()
     print("Alarm", num)
     num += 1
-    sleep(1)
+    sensor.wait_for_no_motion()
+    led.off()
+    #sleep(5)
