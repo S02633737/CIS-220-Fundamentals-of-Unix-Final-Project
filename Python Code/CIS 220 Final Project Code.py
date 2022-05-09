bd3524ec897f41949feda461c904640dd9578965
S02633737 <aanguiano6@student.cccs.edu>
2022-05-09 11:49:00 -0600

    Final Project Code Third Stage
    Third stage of the code for my project, adding the buzzer. I tried using just the normal buzzer class, but for whatever reason, it would only make a clicking noise whenever it activated and not a tone, so I instead used the TonalBuzzer class and set the tone to a normal C4 tone.

--- Python Code/CIS 220 Final Project Code.py
+++ Python Code/CIS 220 Final Project Code.py
@@ -2,17 +2,20 @@
 from time import sleep
 
 #Third Party Imports
-from gpiozero import MotionSensor, LED
+from gpiozero import MotionSensor, LED, TonalBuzzer
 
 sensor = MotionSensor(4)
 led = LED(17)
+buzzer = TonalBuzzer(26)
 
 num = 0
 while True:
     sensor.wait_for_motion()
     led.on()
+    buzzer.play("C4")
     print("Alarm", num)
     num += 1
     sensor.wait_for_no_motion()
     led.off()
+    buzzer.stop()
     #sleep(5)
